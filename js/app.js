54444a8906fdb852436901ecb5448ff53b56f81e
mohsmadi <79091675+mohsmadi@users.noreply.github.com>
2021-02-24 00:17:02 +0200

    Add files via upload
    add click function

--- js/app.js
+++ js/app.js
@@ -2,18 +2,28 @@
 //provide discount beasd on the amount of the meal
 //show up the discount
 
-var Name=prompt("What's Your Name?")
-var age=prompt("How Old are You ?")
+var Name = prompt("What's Your Name?")
+var age = prompt("How Old are You ?")
 
 
 
-var amountOfMeal=prompt("how many meal you have order?")
 
-if (amountOfMeal > 3 && amountOfMeal <=6){
-    alert ("you do have 15% discount!");
-}else if(amountOfMeal >=8 && amountOfMeal < 12){
+var amountOfMeal = prompt("how many meal you have order?")
+
+if (amountOfMeal > 3 && amountOfMeal <= 6) {
+    alert("you do have 15% discount!");
+} else if (amountOfMeal >= 8 && amountOfMeal < 12) {
     alert("You Get 30% Discount!");
-}else if (amountOfMeal == 0){
-}alert (" You Have To Buy More Meal");
+} else if (amountOfMeal == 0) {
+} alert(" You Have To Buy More Meal");
 
-document.getElementById("myHeader").innerHTML = "You Order" + amountOfMeal ;
+
+
+document.getElementById("myHeader").innerHTML = "You Order" + " " + amountOfMeal;
+
+
+document.getElementById("button").onclick = function () {
+    var favouriteColor = prompt("What's Your Favourit color?")
+    document.getElementById("button").style.backgroundColor = favouriteColor;
+
+};
